583e0f91b86b86cdd0565e4ab9d49ea2e804a1c1
Steven Cruz <blackjackers17@gmail.com>
2020-04-04 16:28:28 -0500

    modulo pages, modulo components y modulo pipes

--- src/app/pipes/filtro-correo.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltroCorreoPipe } from './filtro-correo.pipe';
-
-describe('FiltroCorreoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltroCorreoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
